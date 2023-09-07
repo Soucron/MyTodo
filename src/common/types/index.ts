7726efb9ee850f28fe0f384cf38433704cc5e19c
Iniok <ilidanilo@mail.ru>
2023-09-07 23:03:34 +0300

    auth api created, store setup

---
+++ src/common/types/index.ts
@@ -0,0 +1,12 @@
+type FieldErrorType = {
+    error: string,
+    field: string
+}
+
+
+export type BaseResponseType<D = object> = {
+    resultCode: number,
+    messages: string[],
+    data: D,
+    fieldsErrors: FieldErrorType[]
+}
