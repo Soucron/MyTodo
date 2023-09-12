cae63aa60e472596f3e31fa762d64cbf680044cc
Iniok <ilidanilo@mail.ru>
2023-09-12 21:01:34 +0300

    Task component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,27 @@
-
+import {Header} from '../common/components/Header/Header.tsx';
+import { TodolistBar } from '../common/components/TodolistBar/TodolistBar.tsx';
+import {Todolist} from '../features/TodolistsList/Todolist/Todolist.tsx';
 
 
 function App() {
 
-  return (<div>
-      </div>
 
-  )
+
+
+    return (<>
+        <Header/>
+        <div className="h-screen grid grid-cols-6">
+            <TodolistBar/>
+            <div className="grid pt-10 col-start-3 col-end-7 grid-cols-3 gap-3 grid-rows-2   bg-amber-50">
+            <Todolist/>
+            <Todolist/>
+            <Todolist/>
+            <Todolist/>
+            <Todolist/>
+            <Todolist/>
+            </div>
+        </div>
+    </>)
 }
 
 export default App
--- src/features/TodolistsList/Todolist/Task/Task.tsx
+++ src/features/TodolistsList/Todolist/Task/Task.tsx
@@ -1,1 +1,40 @@
+import {useRef, useState} from 'react';
 
+export const Task = () => {
+
+    const [editable,setEditable] = useState<boolean>(false)
+    const [inputValue, setInputValue] = useState<string>('Hello')
+
+    const inputRef = useRef(null)
+
+
+
+    const onKeyDownHandler = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if(e.key === "Enter") {
+            setInputValue(e.currentTarget.value)
+            setEditable(false)
+        }
+    }
+
+        return (
+
+            <div className='flex flex-row gap-3 items-center'>
+                <input type='checkbox' />
+                {editable ? <input
+                        ref={inputRef}
+                        onChange={(e)=> setInputValue(e.target.value)}
+                        onKeyDown={onKeyDownHandler}
+                        value={inputValue}
+                        type="text"
+                        className="w-[220px] h-[30px] rounded text-xl font-medium leading-tight text-black">
+                    </input> :
+                    <span onDoubleClick={()=> setEditable(true)}>
+                                {inputValue}
+                            </span>}
+                <button
+                    className="w-[40px] h-10 row-start-2 bg-red-500 border-black border rounded transform active:scale-75 transition-transform">
+                    <img src="src/common/assets/remove.svg" alt="Delete"/>
+                </button>
+            </div>
+        )
+    }
